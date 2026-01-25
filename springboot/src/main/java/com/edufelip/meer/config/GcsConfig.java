a6473da5dfb1e53f2b84fb3da74c4bfd1da8cf1b
EduSantos <eduardofelipi@gmail.com>
2026-01-24 22:01:36 -0300

    feat(infra): support separate GCS service account via GCS_SERVICE_ACCOUNT_JSON_B64

--- springboot/src/main/java/com/edufelip/meer/config/GcsConfig.java
+++ springboot/src/main/java/com/edufelip/meer/config/GcsConfig.java
@@ -1,7 +1,11 @@
 package com.edufelip.meer.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +13,13 @@ import org.springframework.context.annotation.Configuration;
 public class GcsConfig {
 
   @Bean
-  public Storage storage() {
+  public Storage storage(@Value("${storage.gcs.credentials-path:}") String credentialsPath)
+      throws IOException {
+    if (credentialsPath != null && !credentialsPath.isBlank()) {
+      GoogleCredentials credentials =
+          GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+      return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
     return StorageOptions.getDefaultInstance().getService();
   }
 }
